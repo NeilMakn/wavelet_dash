15934b8e30c5231fd1730dc3486633914738a871
Neilson Maknarakham <nmaknarakham@gmail.com>
2013-04-26 09:20:41 -0700

    Subject: Commiting before merge Author: Neilson -fixed indentation in idle.rb and query.rb

--- jobs/idle.rb
+++ jobs/idle.rb
@@ -1,22 +1,22 @@
 require "./lib/graphite"
 require './lib/data_server_conf'
 
-# last started parkingsessions
+# begin scheduler
 SCHEDULER.every '1s', :first_in => 0 do
   # Create an instance of our helper class
   q = Graphite.new DataServerConf::GRAPHITE
 
-    graphite_test_target = "graphite.com.crowdcompass.vagrant.graphite.cpu.0.idle"
+  graphite_test_target = "graphite.com.crowdcompass.vagrant.graphite.cpu.0.idle"
 
-    # get the current value
-    current_value = q.value graphite_test_target, "-2min"
-    # find set of data preceding current  value
-    data_set = q.points graphite_test_target, "-2min"
-    # find first value's y
-    prev_value = data_set[0][:y]
-    # set threshold
-    threshold_set = 50
+  # get the current value
+  current_value = q.value graphite_test_target, "-2min"
+  # find set of data preceding current  value
+  data_set = q.points graphite_test_target, "-2min"
+  # find first value's y
+  prev_value = data_set[0][:y]
+  # set threshold
+  threshold_set = 50
 
-    # send to dashboard, so the number the meter and the graph widget can understand it
-    send_event 'graphite_test', { current: current_value, last: prev_value, threshold: threshold_set }
+  # send to dashboard, so the number the meter and the graph widget can understand it
+  send_event 'graphite_test', { current: current_value, last: prev_value, threshold: threshold_set }
 end
--- jobs/query.rb
+++ jobs/query.rb
@@ -1,23 +1,23 @@
 require "./lib/graphite"
 require './lib/data_server_conf'
 
-# last started parkingsessions
+# begin scheduler
 SCHEDULER.every '1s', :first_in => 0 do
-    # Create an instance of our helper class
-    q = Graphite.new DataServerConf::GRAPHITE
+  # Create an instance of our helper class
+  q = Graphite.new DataServerConf::GRAPHITE
 
-    graphite_test_target = "carbon.agents.graphite_vagrant_crowdcompass_com-a.cache.queries"
+  graphite_test_target = "carbon.agents.graphite_vagrant_crowdcompass_com-a.cache.queries"
 
-    # get the current value
-    current_value = q.value graphite_test_target, "-5min"
-    # find set of data preceding current  value
-    data_set = q.points graphite_test_target, "-5min"
-    # find first value's y
-    prev_value = data_set[0][:y]
-    # set threshold
-    threshold_set = 300
+  # get the current value
+  current_value = q.value graphite_test_target, "-5min"
+  # find set of data preceding current  value
+  data_set = q.points graphite_test_target, "-5min"
+  # find first value's y
+  prev_value = data_set[0][:y]
+  # set threshold
+  threshold_set = 300
 
 
-    # send to dashboard, so the number the meter and the graph widget can understand it
-    send_event 'query_test', { current: current_value, last: prev_value, threshold: threshold_set }
+  # send to dashboard, so the number the meter and the graph widget can understand it
+  send_event 'query_test', { current: current_value, last: prev_value, threshold: threshold_set }
 end
